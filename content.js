2df70a083abb8993f98bcad4b70f02d02a7c2741
ItsukiKigoshi <67095865+ItsukiKigoshi@users.noreply.github.com>
2022-08-13 18:33:05 +0900

    Delete Comment

--- content.js
+++ content.js
@@ -2,7 +2,6 @@ $(function() {
   $('#ytd-app').remove(); 
   $('#home-page-skeleton').remove();   
   setTimeout(function() {
-    // 0.5秒（500ms）後に処理
     $('tp-yt-app-drawer').remove();
     $('ytd-two-column-browse-results-renderer').remove();
     $('#secondary-inner').remove();
