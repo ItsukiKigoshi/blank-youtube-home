c84a7cb595d51966d6626360926c113c7edbc372
ItsukiKigoshi <67095865+ItsukiKigoshi@users.noreply.github.com>
2022-08-13 18:41:34 +0900

    Add roop

--- content.js
+++ content.js
@@ -1,11 +1,11 @@
 $(function() {
-  $('#ytd-app').remove(); 
-  $('#home-page-skeleton').remove();   
-  setTimeout(function() {
-    $('tp-yt-app-drawer').remove();
-    $('ytd-two-column-browse-results-renderer').remove();
-    $('#secondary-inner').remove();
-    $('ytd-item-section-renderer').remove();
-    $('ytd-comments-entry-point-header-renderer').remove();
-	}, 500);
+  $('tp-yt-app-drawer').remove();
+  $('ytd-two-column-browse-results-renderer').remove();
+  $('#home-page-skeleton').remove();
+  for (var i=0; i<1000; i++){
+    setTimeout(function() {
+      $('tp-yt-app-drawer').remove();
+      $('ytd-two-column-browse-results-renderer').remove();
+    }, 100);
+  } 
 });
